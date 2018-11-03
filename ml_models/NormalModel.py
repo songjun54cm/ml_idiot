be4f024f6f3be562f0f55e75671080318cd5be3b
songjun <songjun@corp.netease.com>
2018-11-03 13:18:41 +0800

    x

---
+++ ml_models/NormalModel.py
@@ -0,0 +1,12 @@
+__author__ = 'JunSong<songjun@corp.netease.com>'
+# Date: 2018/11/2
+import argparse
+from ml_models.BasicModel import BasicModel
+
+class NormalModel(BasicModel):
+    def __init__(self, config):
+        super(NormalModel, self).__init__(config)
+
+    def create(self,config):
+        print("do nothing, pass.")
+        pass
